0bfa92c56287ab7469d1b627a2aa359263df2356
Farhan Ahmed <farhan1086@gmail.com>
2019-10-31 17:26:30 +0530

    final touch

---
+++ RenderPartialView_Demo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using RenderPartialView_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RenderPartialView_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CustomerContext db = new CustomerContext();
+
+        public ActionResult Index()
+        {
+            var customers = db.Customers.ToList();
+            return View(customers);
+        }
+
+        public PartialViewResult Customers()
+        {
+            var customers = db.Customers.ToList();
+            return PartialView("_Customers",customers);
+        }
+    }
+}
